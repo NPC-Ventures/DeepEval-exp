388b876e97b7ed6b857b0bbf724b71272e7d20a9
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2026-02-04 02:38:10 +0530

    add deepeval assert_test

--- deepeval_tests/test_meeting_summarizer.py
+++ deepeval_tests/test_meeting_summarizer.py
@@ -8,8 +8,9 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.meeting_summarizer.meeting_summarizer import MeetingSummarizer
 from deepeval import evaluate
-import json
 from src.utils.evaluator import Evaluator
+from deepeval import assert_test
+import pytest
 
 documents_path = "src/meeting_summarizer/transcripts"
 transcripts = []
@@ -33,41 +34,33 @@ dataset = EvaluationDataset(goldens=goldens)
 
 
 summarizer = MeetingSummarizer()  # Initialize with your best config
-summary_test_cases = []
-action_item_test_cases = []
-for golden in dataset.goldens:
+
+
+@pytest.mark.parametrize("golden", dataset.goldens)
+def test(golden: Golden):
     summary, action_items = summarizer.summarize(golden.input)
-    print("*" * 40)
-    print(summary)
-    print("*" * 40)
-    print("JSON:")
-    print(json.dumps(action_items, indent=2))
-    print("*" * 40)
     summary_test_case = LLMTestCase(input=golden.input, actual_output=summary)
     action_item_test_case = LLMTestCase(
         input=golden.input, actual_output=str(action_items)
     )
-    summary_test_cases.append(summary_test_case)
-    action_item_test_cases.append(action_item_test_case)
 
-evaluator = Evaluator()
+    evaluator = Evaluator()
 
-summary_concision = evaluator.build_metric(
-    name="Summary Concision",
-    # Write your criteria here
-    criteria="Assess whether the summary is concise and focused only on the essential points of the meeting? It should avoid repetition, irrelevant details, and unnecessary elaboration.",
-    threshold=0.9,
-    evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
-)
+    summary_concision = evaluator.build_metric(
+        name="Summary Concision",
+        # Write your criteria here
+        criteria="Assess whether the summary is concise and focused only on the essential points of the meeting? It should avoid repetition, irrelevant details, and unnecessary elaboration.",
+        threshold=0.9,
+        evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+    )
 
-action_item_check = evaluator.build_metric(
-    name="Action Item Accuracy",
-    # Write your criteria here
-    criteria="Are the action items accurate, complete, and clearly reflect the key tasks or follow-ups mentioned in the meeting?",
-    threshold=0.9,
-    evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
-)
+    action_item_check = evaluator.build_metric(
+        name="Action Item Accuracy",
+        # Write your criteria here
+        criteria="Are the action items accurate, complete, and clearly reflect the key tasks or follow-ups mentioned in the meeting?",
+        threshold=0.9,
+        evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+    )
 
-evaluate(test_cases=summary_test_cases, metrics=[summary_concision])
-
-# evaluate(test_cases=action_item_test_cases, metrics=[action_item_check])
+    assert_test(test_case=summary_test_case, metrics=[summary_concision])
+    assert_test(test_case=action_item_test_case, metrics=[action_item_check])
